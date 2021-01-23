a6da0c132a2cd85cba93d0c1a57bbbee4fb41d3f
ElegantSoft <react.laravel.dev@gmail.com>
2021-01-23 17:24:58 +0200

    break if face blocked

--- src/services/fb.service.ts
+++ src/services/fb.service.ts
@@ -17,30 +17,34 @@ export class FbService {
     const [pageId, postId] = this.formatterService.getPagePostIds(postUrl);
     let data: Comment[];
     let next: string;
-    const response = await this.httpClientService.get<CommentPaging>(
-      `${process.env.API}/${pageId}_${postId}/comments?access_token=${process.env.TOKEN}&limit=25`
-    );
-    data = response.data.data;
-    next = response.data.paging.next;
-    comments.push(...data);
-    while (next) {
-      try {
-        const nextResponse = await this.httpClientService.get<CommentPaging>(
-          next
-        );
-        comments.push(...nextResponse.data.data);
-        next = nextResponse.data.paging.next;
-        process.stdout.clearLine(0); // clear current text
-        process.stdout.cursorTo(0); // move cursor to beginning of line
-        process.stdout.write(
-          `${colors.FgBlue} number of comments ${comments.length}`
-        );
-      } catch (e) {
-        console.warn("error while get comments", e);
-        console.log(colors.FgRed, "FACEBOOK BLOCKED YOU ");
-        next = null;
+    try {
+      const response = await this.httpClientService.get<CommentPaging>(
+        `${process.env.API}/${pageId}_${postId}/comments?access_token=${process.env.TOKEN}&limit=25`
+      );
+      data = response.data.data;
+      next = response.data.paging.next;
+      comments.push(...data);
+      while (next) {
+        try {
+          const nextResponse = await this.httpClientService.get<CommentPaging>(
+            next
+          );
+          comments.push(...nextResponse.data.data);
+          next = nextResponse.data.paging.next;
+          process.stdout.clearLine(0); // clear current text
+          process.stdout.cursorTo(0); // move cursor to beginning of line
+          process.stdout.write(
+            `${colors.FgBlue} number of comments ${comments.length}`
+          );
+        } catch (e) {
+          console.warn("error while get comments", e);
+          console.log(colors.FgRed, "FACEBOOK BLOCKED YOU ");
+          next = null;
+        }
       }
+      return comments;
+    } catch (e) {
+      console.log(colors.FgRed, "FACEBOOK BLOCKED YOU ");
     }
-    return comments;
   }
 }
